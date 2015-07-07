2590a111b541195dcbc08373bf4cc4bc9567932c
gdouglas <grm.dgs@gmail.com>
2015-07-07 10:49:15 -0700

    setup slider on homepage
    slide is now working on homepage with some CSS tweaks to colours.

---
+++ teams/js/slider.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function ($) {
+
+    var _SlideshowTransitions = [
+        //Fade
+        { $Duration: 1200, $Opacity: 2 }
+    ];
+
+    var options = { 
+            $AutoPlay: true,
+            $FillMode:4,
+            $AutoPlayInterval:3000,
+            $SlideDuration:1200,
+
+            $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+                $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
+                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                $Scale: false                                   //Scales bullets navigator or not while slider scale
+            },
+
+            $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
+                $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                $SpacingX: 12,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                $SpacingY: 4,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                $Orientation: 1,                                //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+                $Scale: false                                   //Scales bullets navigator or not while slider scale
+            }                        
+        };
+
+
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+	/*add controls*/
+    $('#slider-controls .pause').click(function(){
+    	jssor_slider1.$Pause();
+    	$('#slider-controls span').toggleClass("active");
+    });
+    $('#slider-controls .play').click(function(){
+    	jssor_slider1.$Play();
+    	$('#slider-controls span').toggleClass("active");
+    });
+});
