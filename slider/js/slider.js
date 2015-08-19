f84e941419aa8bcd0695b31477a65449421746d2
gdouglas <grm.dgs@gmail.com>
2015-08-19 13:18:34 -0700

    Created base version of slider
    Have a basic working version of the slider separate from the masterpage

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -34,41 +34,13 @@ jQuery(document).ready(function ($) {
 
 
     var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-
-        //responsive code begin
-        //you can remove responsive code if you don't want the slider scales
-        //while window resizes
-        /*
-
-        function ScaleSlider() {
-            var parentWidth = $('#slider1_container').parent().width();
-            if (parentWidth) {
-                jssor_slider1.$ScaleWidth(parentWidth);
-            }
-            else
-                window.setTimeout(ScaleSlider, 30);
-        }
-        //Scale slider after document ready
-        ScaleSlider();
-                                        
-        //Scale slider while window load/resize/orientationchange.
-        $(window).bind("load", ScaleSlider);
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
-        
-        */
-        //responsive code end
-
-
-
-
-	/*add controls*/
+    /*add controls*/
     $('#slider-controls .pause').click(function(){
-    	jssor_slider1.$Pause();
-    	$('#slider-controls span').toggleClass("active");
+        jssor_slider1.$Pause();
+        $('#slider-controls span').toggleClass("active");
     });
     $('#slider-controls .play').click(function(){
-    	jssor_slider1.$Play();
-    	$('#slider-controls span').toggleClass("active");
+        jssor_slider1.$Play();
+        $('#slider-controls span').toggleClass("active");
     });
 });
