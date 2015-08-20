403d8e7d285c0bc0039653361e54995b87e5d3dd
gdouglas <grm.dgs@gmail.com>
2015-08-20 16:15:53 -0700

    CSS tweaks and html adjustments
    Slider is now in line with design and html standards, using data
    attributes.

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function ($) {
             $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
                 $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
                 $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-                $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $AutoCenter: 0,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
                 $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
                 $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
                 $SpacingX: 12,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
@@ -34,13 +34,5 @@ jQuery(document).ready(function ($) {
 
 
     var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-    /*add controls*/
-    $('#slider-controls .pause').click(function(){
-        jssor_slider1.$Pause();
-        $('#slider-controls span').toggleClass("active");
-    });
-    $('#slider-controls .play').click(function(){
-        jssor_slider1.$Play();
-        $('#slider-controls span').toggleClass("active");
-    });
+
 });
