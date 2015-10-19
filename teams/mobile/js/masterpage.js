7ab151d3c11cb720218e35edf4b67e6a3a77631c
gdouglas <grm.dgs@gmail.com>
2015-10-19 16:25:16 -0700

    Added some viewport manipulation

--- teams/mobile/js/masterpage.js
+++ teams/mobile/js/masterpage.js
@@ -1,7 +1,19 @@
+/*position document at content *
+*
+*
+*
+*
+*needs to be done only for mobile*/
 jQuery(document).ready(function($){
-   console.log('ready');
+    $(document).scrollTop($(".breadcrumb").offset().top-20);
 });
 
+/*
+*
+*
+*Navbar Controls
+*
+*/
 $(".navbar-toggle").click(function() 
 {  
     // hides children divs if shown, shows if hidden 
@@ -9,4 +21,23 @@ $(".navbar-toggle").click(function()
     jQuery('#side-nav').slideToggle();
 });
 
+/*
+*
+*
+*Smooth scrolling on bookmarks
+*
+*/
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
 
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
