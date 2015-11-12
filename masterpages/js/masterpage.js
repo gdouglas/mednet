a57898dd7a2ab3e52885dfe182fa05e089c8d70f
gdouglas <grm.dgs@gmail.com>
2015-11-12 13:43:21 -0800

    Various tweak to mobile layout

---
+++ masterpages/js/masterpage.js
@@ -0,0 +1,83 @@
+/*
+*
+*
+*Navbar Controls
+*
+*/
+$(".navbar-toggle").click(function() 
+{  
+    // hides children divs if shown, shows if hidden 
+    $('#navbar').slideToggle();
+    jQuery('#side-nav').slideToggle();
+});
+
+/*
+*
+*
+*Smooth scrolling on bookmarks
+*
+*/
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('#s4-workspace').animate({
+                 scrollTop: target.offset().top
+            }, 'fast');
+            return false;
+        }
+    }
+});
+
+
+/*
+*
+*
+*Fix back to top at top of footer
+*
+*/
+function checkOffset() {
+    if($('.toplink').offset().top + $('.toplink').height()>= $('footer').offset().top - 10){
+        bottomTopLink();
+    }//end if
+    if($(document).scrollTop() + window.innerHeight < $('footer').offset().top){
+        fixTopLink();
+    }//end if
+}//end checkOffset
+
+function defaultTopLink(){
+    $('.toplink').css({
+        'position':'static'
+    });
+}
+
+function bottomTopLink(){
+    $('.toplink').css({
+        'position': 'absolute',
+        'bottom': '22px',
+        'background': '#3868a6'
+    });
+}//end defaultTopLink
+
+function fixTopLink(){
+    console.log("fix");
+    $('.toplink').css({
+        'position': 'fixed',
+        'bottom': '0',
+        'background':'#002145'
+    }); // restore when you scroll up
+}//end fixTopLink
+
+
+/*set timer to compensate for safari ios scrolling issue*/
+setInterval(function(){
+    if ($(window).width() < 768) {
+        checkOffset(); 
+    }else{
+        defaultTopLink();
+    }
+
+},100);
