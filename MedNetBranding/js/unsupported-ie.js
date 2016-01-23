f73ba45eb84d4a144f85f422d693e4de6e890375
gdouglas <grm.dgs@gmail.com>
2016-01-22 17:20:36 -0800

    Updated images
    -unsupported IE now has icons, message updated
    -replaced SVG with png and gif

--- MedNetBranding/js/unsupported-ie.js
+++ MedNetBranding/js/unsupported-ie.js
@@ -19,10 +19,9 @@ function getCookie(cname) {
 
 function checkCookie() {
     var medNetAlert = getCookie("medNetAlert");
-    medNetAlert = "alert";
     if (medNetAlert == "alerted today") {
         //do not show alert
-        alert(medNetAlert);
+        console.log(medNetAlert);
     } else {
         medNetAlert = "alerted today";
         showAlert();
@@ -31,7 +30,29 @@ function checkCookie() {
 
 function showAlert() {
     var browserAlertDiv = document.createElement('div');
-    browserAlertDiv.innerHTML = "<div id='browser-alert'><div class='alert-wrapper'><p>You are using internet explorer 7 which is an outdated browser. MedNet does not support this browser which will result in you having a inferior and insecure experience. To use MedNet please use a <a href='#' target = '_blank'>recommended browser</a></p><p>Thank you</p><span id='close'>X</span></div></div>";
+    browserAlertDiv.innerHTML = 
+    "<div id='browser-alert'>"+
+        "<div class='alert-wrapper'>"+
+            "<p>You are using Internet Explorer 7 or 8 which is an outdated browser. For the best experience on MedNet, please use one of the following browsers:</p>"+
+            "<table class='icons'>"+
+            "  <tr>"+
+            "    <th><image src='~SiteCollection/_layouts/15/MedNetBranding/images/ie.png' class='browser-icon'></th>"+
+            "    <th><image src='~SiteCollection/_layouts/15/MedNetBranding/images/chrome.png' class='browser-icon'></th>"+
+            "    <th><image src='~SiteCollection/_layouts/15/MedNetBranding/images/ff.png' class='browser-icon'></th>"+
+            "    <th><image src='~SiteCollection/_layouts/15/MedNetBranding/images/safari.png' class='browser-icon'></th>"+
+            "  </tr>"+
+            "  <tr class='titles'>"+
+            "    <td>Internet Explorer<br>Version 9+</td>"+
+            "    <td>Chrome</td>"+
+            "    <td>Firefox</td>"+
+            "    <td>Safari</td>"+
+            "  </tr>"+
+            "</table>"+
+            "<p><strong>Need help?</strong><br>"+
+            "If you have any questions or to get support upgrading or installing a new browser, please contact the MedIT Service Desk at 1-877-266-0666 or <a href='mailto:medit.servicedesk@ubc.ca'>medit.servicedesk@ubc.ca</a>.</p>"+
+            "<span id='close'>X</span>"+
+        "</div>"+
+    "</div>";
     browserAlertDiv.class = "browser-alert";
 
     $('#browser-alert').remove();
@@ -47,3 +68,4 @@ function addCloseListener() {
 $(document).ready(function() {
     checkCookie();
 });
+
