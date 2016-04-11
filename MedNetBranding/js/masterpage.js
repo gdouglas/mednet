825b7747a7386e0dec944e20f885ae198880beea
gdouglas <grm.dgs@gmail.com>
2016-04-11 15:32:25 -0700

    removed smooth scroll
    -only now applies to back to top button

--- MedNetBranding/js/masterpage.js
+++ MedNetBranding/js/masterpage.js
@@ -52,18 +52,20 @@ function addListeners() {
  *
  */
 function addSmoothScroll() {
-    $('a[href*=#]:not([href=#],#accordion a[href*=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('#s4-workspace').animate({
-                    scrollTop: target.offset().top
-                }, 'fast');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('#s4-workspace').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
                 return false;
             }
         }
-    });
+    });    
 }
 
 /*
